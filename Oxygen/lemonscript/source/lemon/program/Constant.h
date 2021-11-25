365971c57830385c52d0099773cefa40d71f53da
Eukaryot <waymore024@gmail.com>
2021-11-25 19:59:31 +0100

    Added a missing header

---
+++ Oxygen/lemonscript/source/lemon/program/Constant.h
@@ -0,0 +1,32 @@
+/*
+*	Part of the Oxygen Engine / Sonic 3 A.I.R. software distribution.
+*	Copyright (C) 2017-2021 by Eukaryot
+*
+*	Published under the GNU GPLv3 open source software license, see license.txt
+*	or https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
+#pragma once
+
+#include "lemon/compiler/Token.h"
+
+
+namespace lemon
+{
+
+	class API_EXPORT Constant
+	{
+	friend class Module;
+
+	public:
+		inline const std::string& getName() const  { return mName; }
+		inline const DataTypeDefinition* getDataType() const  { return mDataType; }
+		inline uint64 getValue() const  { return mValue; }
+
+	private:
+		std::string mName;
+		const DataTypeDefinition* mDataType = nullptr;
+		uint64 mValue = 0;
+	};
+
+}
