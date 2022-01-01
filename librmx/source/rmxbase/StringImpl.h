eb029222cb46971c17999e95b3015f2b54c5e176
Eukaryot <waymore024@gmail.com>
2022-01-01 11:10:45 +0100

    Happy New Year 2022

--- librmx/source/rmxbase/StringImpl.h
+++ librmx/source/rmxbase/StringImpl.h
@@ -1,6 +1,6 @@
 /*
 *	rmx Library
-*	Copyright (C) 2008-2021 by Eukaryot
+*	Copyright (C) 2008-2022 by Eukaryot
 *
 *	Published under the GNU GPLv3 open source software license, see license.txt
 *	or https://www.gnu.org/licenses/gpl-3.0.en.html
