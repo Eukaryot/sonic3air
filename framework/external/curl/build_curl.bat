42c578d402225a743776588118176dd5d32a2a8d
Eukaryot <waymore024@gmail.com>
2022-12-14 19:44:47 +0100

    Fix for building libcurl

--- framework/external/curl/build_curl.bat
+++ framework/external/curl/build_curl.bat
@@ -13,10 +13,10 @@ call ../get_msbuild_path.bat
 
 pushd curl
 
-pushd curl\projects\Windows\VC14.30
+pushd projects\Windows\VC14.30
 set _CL_=/MT
-%msbuildPath22% curl-all.sln /target:libcurl /property:Configuration="LIB Release - DLL Windows SSPI" /property:Platform=Win32 -verbosity:minimal
-%msbuildPath22% curl-all.sln /target:libcurl /property:Configuration="LIB Release - DLL Windows SSPI" /property:Platform=x64   -verbosity:minimal
+%msbuildPath22% curl-all.sln /target:libcurl /property:Configuration="LIB Release - DLL Windows SSPI" /property:Platform=Win32 /p:RuntimeLibrary=MT_StaticRelease -verbosity:minimal
+%msbuildPath22% curl-all.sln /target:libcurl /property:Configuration="LIB Release - DLL Windows SSPI" /property:Platform=x64   /p:RuntimeLibrary=MT_StaticRelease -verbosity:minimal
 set _CL_=
 popd
 
