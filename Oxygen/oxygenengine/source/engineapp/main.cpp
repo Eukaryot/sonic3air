1415c22dda789c190158fd6052004839a4bf6685
Eukaryot <waymore024@gmail.com>
2023-01-23 19:08:10 +0100

    Updated copyright notices to 2023

--- Oxygen/oxygenengine/source/engineapp/main.cpp
+++ Oxygen/oxygenengine/source/engineapp/main.cpp
@@ -1,3 +1,11 @@
+/*
+*	Part of the Oxygen Engine / Sonic 3 A.I.R. software distribution.
+*	Copyright (C) 2017-2023 by Eukaryot
+*
+*	Published under the GNU GPLv3 open source software license, see license.txt
+*	or https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
 #define RMX_LIB
 #include "engineapp/pch.h"
 #include "engineapp/EngineDelegate.h"
