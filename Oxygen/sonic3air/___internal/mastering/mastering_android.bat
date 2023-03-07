127b70e3fd0654584ee0a5681123c2071d0bc85e
Eukaryot <waymore024@gmail.com>
2023-03-07 20:46:45 +0100

    For Android, leave out the remastered soundtrack by default, as we have a downloader for that

--- Oxygen/sonic3air/___internal/mastering/mastering_android.bat
+++ Oxygen/sonic3air/___internal/mastering/mastering_android.bat
@@ -10,8 +10,11 @@ set outputDir=build\_android\app\src\main\assets
 rmdir "%outputDir%" /s /q
 mkdir "%outputDir%"
 
-robocopy "_master_image_template\data" "%outputDir%/data" /mir
+robocopy "_master_image_template\data" "%outputDir%\data" /mir
 copy "_master_image_template\config.json" "%outputDir%"
+
+:: Leave out the remastered music, as the Android build can use an internal downloader
+del "%outputDir%\data\audioremaster.bin"
 
 popd
 
