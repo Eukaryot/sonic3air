e6fce95d9fef90f0409379642c855ed37dfb2f64
Eukaryot <waymore024@gmail.com>
2024-12-07 19:45:54 +0100

    More purging of the old enduser build differentiation

--- Oxygen/sonic3air/___internal/mastering/mastering_windows_32bit.bat
+++ Oxygen/sonic3air/___internal/mastering/mastering_windows_32bit.bat
@@ -18,7 +18,7 @@ mkdir "%outputDir%"
 :: Make sure the needed binaries are all up-to-date
 
 %msbuildPath% ..\oxygenengine\build\_vstudio\oxygenengine.sln /property:Configuration=Release /property:Platform=Win32 -verbosity:minimal
-%msbuildPath% build\_vstudio\sonic3air.sln /property:Configuration=Release-Enduser /property:Platform=Win32 -verbosity:minimal
+%msbuildPath% build\_vstudio\sonic3air.sln /property:Configuration=Release /property:Platform=Win32 -verbosity:minimal
 
 
 
@@ -29,7 +29,7 @@ robocopy "_master_image_template" "%outputDir%" /e
 :: Leave out the remastered music, as the Windows build can use an internal downloader
 del "%outputDir%\data\audioremaster.bin"
 
-copy "bin\Release-Enduser_x86\*.exe" "%outputDir%"
+copy "bin\Release_x86\*.exe" "%outputDir%"
 copy "source\external\discord_game_sdk\lib\x86\discord_game_sdk.dll" "%outputDir%"
 
 
@@ -56,8 +56,8 @@ popd
 
 :: Archive artifacts
 mkdir "%destDir%\artifacts\bin"
-copy "bin\Release-Enduser_x86\*.exe" "%destDir%\artifacts\bin"
-copy "bin\Release-Enduser_x86\*.pdb" "%destDir%\artifacts\bin"
+copy "bin\Release_x86\*.exe" "%destDir%\artifacts\bin"
+copy "bin\Release_x86\*.pdb" "%destDir%\artifacts\bin"
 
 pushd "%destDir%"
 	"C:\Program Files\7-Zip\7z.exe" a -t7z -mx1 -r artifacts.7z artifacts
