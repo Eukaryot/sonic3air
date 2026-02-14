1f753652d887cc38761f2d62953e3df7141f15f8
Eukaryot <waymore024@gmail.com>
2026-02-14 13:12:29 +0100

    Updated copyright to 2026

--- Oxygen/oxygenengine/source/oxygen/menu/devmode/windows/ScriptBuildWindow.h
+++ Oxygen/oxygenengine/source/oxygen/menu/devmode/windows/ScriptBuildWindow.h
@@ -1,6 +1,6 @@
 ﻿/*
 *	Part of the Oxygen Engine / Sonic 3 A.I.R. software distribution.
-*	Copyright (C) 2017-2025 by Eukaryot
+*	Copyright (C) 2017-2026 by Eukaryot
 *
 *	Published under the GNU GPLv3 open source software license, see license.txt
 *	or https://www.gnu.org/licenses/gpl-3.0.en.html
